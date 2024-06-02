3ad3a527f4162a4bb6f7e18aff7059d27544da1d
uneap@sk.com <wjdekdms9@sookmyung.ac.kr>
2024-06-03 01:49:48 +0900

    add delete, update, insert method in brand storage

--- domain/brand/src/test/java/com/cody/domain/brand/TestConfiguration.java
+++ domain/brand/src/test/java/com/cody/domain/brand/TestConfiguration.java
@@ -2,7 +2,7 @@ package com.cody.domain.brand;
 
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication(scanBasePackages = {"com.cody.domain.brand", "com.cody.resource.db"})
+@SpringBootApplication(scanBasePackages = {"com.cody.domain.brand", "com.cody.resource.db", "com.cody.common.core"})
 public class TestConfiguration {
 
 }
