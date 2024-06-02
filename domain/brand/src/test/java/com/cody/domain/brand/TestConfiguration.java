ee60f1fb86b4eb45e79531e3fc6652d54ad225df
uneap@sk.com <wjdekdms9@sookmyung.ac.kr>
2024-06-02 15:18:47 +0900

    add brand domain test

---
+++ domain/brand/src/test/java/com/cody/domain/brand/TestConfiguration.java
@@ -0,0 +1,8 @@
+package com.cody.domain.brand;
+
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication(scanBasePackages = {"com.cody.domain.brand", "com.cody.resource.db"})
+public class TestConfiguration {
+
+}
