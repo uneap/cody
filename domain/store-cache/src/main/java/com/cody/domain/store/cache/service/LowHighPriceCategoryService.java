4df88e3eaa045a3b1d5a1fcfa31d2ff109e624ed
uneap@sk.com <wjdekdms9@sookmyung.ac.kr>
2024-06-04 01:18:54 +0900

    add service for 3 Requirements

--- domain/store-cache/src/main/java/com/cody/domain/store/cache/service/LowHighPriceCategoryService.java
+++ domain/store-cache/src/main/java/com/cody/domain/store/cache/service/LowHighPriceCategoryService.java
@@ -1,5 +1,7 @@
 package com.cody.domain.store.cache.service;
 
+import static com.cody.domain.store.cache.constants.constants.PRICE_CATEGORY;
+
 import com.cody.domain.store.cache.dto.DisplayProduct;
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.redis.core.RedisTemplate;
@@ -9,8 +11,19 @@ import org.springframework.stereotype.Service;
 @RequiredArgsConstructor
 public class LowHighPriceCategoryService {
     private final RedisTemplate<String, DisplayProduct> redisDisplayProductTemplate;
-
-    public String getLowHighPriceCategory(long categoryId) {
-        return String.format("priceCategory:%d", categoryId);
+    public void refreshLowHighCategoryToAdd(DisplayProduct displayProduct) {
+        redisDisplayProductTemplate.opsForZSet().add(getLowHighPriceCategory(displayProduct.getCategoryId()), displayProduct, displayProduct.getProductPrice());
     }
+
+    public void refreshLowHighCategoryToDelete(DisplayProduct displayProduct) {
+        redisDisplayProductTemplate.opsForZSet().remove(getLowHighPriceCategory(displayProduct.getCategoryId()), displayProduct);
+    }
+
+    public void refreshLowHighCategoryToUpdate(DisplayProduct displayProduct) {
+        redisDisplayProductTemplate.opsForZSet().add(getLowHighPriceCategory(displayProduct.getCategoryId()), displayProduct, displayProduct.getProductPrice());
+    }
+    private String getLowHighPriceCategory(long categoryId) {
+        return String.format(PRICE_CATEGORY, categoryId);
+    }
+
 }
