3ca18a23d8cf78f8bead476977220c53fed9d79b
uneap@sk.com <wjdekdms9@sookmyung.ac.kr>
2024-06-05 23:06:30 +0900

    add full-cache-batch for allUser

--- backend/full-cache-batch/src/main/java/com/cody/backend/full/cache/batch/steps/AllUserReader.java
+++ backend/full-cache-batch/src/main/java/com/cody/backend/full/cache/batch/steps/AllUserReader.java
@@ -1,8 +1,46 @@
 package com.cody.backend.full.cache.batch.steps;
 
+import static com.cody.backend.full.cache.batch.constant.constants.DISPLAY_PRODUCT_READER;
+import static com.cody.backend.full.cache.batch.constant.constants.FETCH_SIZE;
+import static java.time.format.DateTimeFormatter.ISO_LOCAL_DATE_TIME;
+
+import com.cody.backend.full.cache.batch.constant.Query;
+import com.cody.domain.store.cache.dto.AllUser;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.time.LocalDateTime;
+import javax.sql.DataSource;
+import org.springframework.batch.item.database.JdbcCursorItemReader;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.jdbc.core.BeanPropertyRowMapper;
+import org.springframework.jdbc.core.RowMapper;
 import org.springframework.stereotype.Component;
 
 @Component
-public class AllUserReader  {
+public class AllUserReader extends JdbcCursorItemReader<AllUser> {
 
+    public AllUserReader(@Qualifier("dataSource") DataSource dataSource) {
+        setName(DISPLAY_PRODUCT_READER);
+        setFetchSize(FETCH_SIZE);
+        setDataSource(dataSource);
+        setRowMapper(new BeanPropertyRowMapper<>(AllUser.class));
+        setSql(Query.getAllUserQuery());
+        setRowMapper(new AllUserReaderRowMapper());
+        setDriverSupportsAbsolute(true);
+        setVerifyCursorPosition(false);
+    }
+
+    private static class AllUserReaderRowMapper implements RowMapper<AllUser> {
+        @Override
+        public AllUser mapRow(ResultSet rs, int rowNum) throws SQLException {
+            return AllUser.builder()
+                          .userId(rs.getLong("user_id"))
+                          .userName(rs.getString("user_name"))
+                          .sellerId(rs.getLong("seller_id"))
+                          .sellerName(rs.getString("seller_name"))
+                          .lastUpdatedDateTime(
+                              LocalDateTime.parse(ISO_LOCAL_DATE_TIME.format(LocalDateTime.now())))
+                          .build();
+        }
+    }
 }
--- domain/store-cache/src/main/java/com/cody/domain/store/cache/constants/constants.java
+++ domain/store-cache/src/main/java/com/cody/domain/store/cache/constants/constants.java
@@ -2,4 +2,5 @@ package com.cody.domain.store.cache.constants;
 
 public class constants {
     public static final String PRICE_CATEGORY = "priceCategory:%d";
+    public static final String ALL_USER = "allUser:%d";
 }
